750a4ae23d3655f35c013101e7e08ecb31709069
Emil Ehnström <emil.ehnstrom@gispo.fi>
2026-08-26 09:49:45 +0300

    remove render scripts

--- render.ps1
+++
@@ -1,4 +0,0 @@
-docker run -it --rm -v "${pwd}/src:/app".ToLower() -v "${pwd}/out:/out".ToLower() gispo/bookdown:latest bash -c "set -e \
-rm -rf /out/* \
-Rscript -e \`"bookdown::render_book('/app', 'bookdown::html_book')\`" \
-mv /app/_book/* /out/ && rm -r /app/_book && chmod --recursive 777 /out"
--- render.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm -v "$(pwd)/src:/app" -v "$(pwd)/out:/out" gispo/bookdown:latest /app/run.sh
